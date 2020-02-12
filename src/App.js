9d074ca9a53de78bbc11bc153a17c5bbb030c645
Jordan Haagenson <jdhaagenson@gmail.com>
2020-02-12 13:07:49 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./index.css";
 import todosList from "./todos.json";
+import TodoList from "./TodoList.js";
 
 
 class App extends Component {
@@ -68,49 +69,6 @@ class App extends Component {
           </span>
           <button onClick={this.handleClearClick} className="clear-completed">Clear completed</button>
         </footer>
-      </section>
-    );
-  }
-}
-
-class TodoItem extends Component {
-  render() {
-    return (
-      <li className={this.props.completed ? "completed" : ""}>
-        <div className="view">
-          <input
-            className="toggle"
-            type="checkbox"
-            checked={this.props.completed}
-            onChange = {this.props.handleToggle}
-          />
-          <label>{this.props.title}</label>
-          <button className="destroy"
-          onClick={this.props.handleDelete}/>
-        </div>
-      </li>
-    );
-  }
-}
-
-class TodoList extends Component {
-  render() {
-    return (
-      <section className="main">
-        <ul className="todo-list">
-          {this.props.todos.map(todo => (
-            <TodoItem
-            handleClear = {event =>
-            this.props.handleClear(todo.id)}
-            handleToggle={event =>
-            this.props.handleToggle(todo.id)}
-            handleDelete = {event =>
-            this.props.handleDelete(todo.id)}
-            title={todo.title}
-            completed={todo.completed}
-            />
-          ))}
-        </ul>
       </section>
     );
   }
